e7cc0ae9dd211d484ddd197daec5e9bf53474e81
selkieslife <aham.brahmasmi.mumbai@gmail.com>
2025-06-18 18:33:22 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+
 // pages/_app.tsx
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
 
