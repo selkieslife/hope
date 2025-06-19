89d950b005ef188b2ce7da3ee2c3e45f912a479d
selkieslife <aham.brahmasmi.mumbai@gmail.com>
2025-06-19 10:55:51 +0530

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://your-project.supabase.co'
-const supabaseAnonKey = 'your-anon-key'
+const supabaseUrl = 'https://wsuptvmeejvxyipazpip.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
